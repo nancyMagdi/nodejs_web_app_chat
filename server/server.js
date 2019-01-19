985dd984893bf98c5e708519d1c6484dc4f5fd13
Nancy Magdi <nancy.magdi@innuva.com>
2019-01-19 20:18:00 +0200

    adding cors headers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
 
 var express = require('express');
+var cors = require('cors')
+
 var app = express();
+app.use(cors());
 var bodyParser = require('body-parser');
 app.use(bodyParser.json())
  
 require('./app/router/router.js')(app);
 
 const db = require('./app/config/db.config.js');
- 
+ // Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // Create a Server
 var server = app.listen(3300, function () {
  
