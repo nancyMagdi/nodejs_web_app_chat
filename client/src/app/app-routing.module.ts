f559bac6d4388be54465dd3fb7628526bc800dfc
Nancy Magdi <nancy.magdi@innuva.com>
2019-01-22 22:21:34 +0200

    adding the chat route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { SignupComponent } from "./components/signup/signup.component";
 import { SharedModule } from './shared/shared.module';
+import { MainContainerComponent } from './components/main-container/main-container.component';
 
 const routes: Routes = [
   { path: "", component: LoginComponent },
   { path: "signup", component: SignupComponent },
+  {path:"chat" , component:MainContainerComponent}
 ];
 //, canActivate: [AuthGuard] 
 @NgModule({
