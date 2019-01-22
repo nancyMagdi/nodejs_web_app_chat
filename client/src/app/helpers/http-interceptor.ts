546f1564fa0c2e04b8ed8dde605aaa81e61de568
Nancy Magdi <nancy.magdi@innuva.com>
2019-01-22 22:14:52 +0200

    Adding the interceptor to add the token in header

--- client/src/app/helpers/http-interceptor.ts
+++ client/src/app/helpers/http-interceptor.ts
@@ -16,15 +16,13 @@ export class HttpInterceptorHelper implements HttpInterceptor {
     public apiUrl: String = environment.apiUrl;
     constructor(private injector: Injector) { }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // add token to request headers        
-         // get the token from a service
-         /*
-         const token: string = localStorage.getItem('token');
-
+        // add token to request headers              
+         let token  = localStorage.getItem('token');
+        
          // add it if we have one
          if (token) {
             request = request.clone({ headers: request.headers.set('x-access-token', token) });
-         }        */
+         }        
         const url = this.apiUrl + request.url;
         if (!request.headers.has('Content-Type')) {
             request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
